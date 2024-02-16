dab4528582bf356a0e3602ffa2535c580570d58e
mr-AnandShinde-358 <97289992+mr-AnandShinde-358@users.noreply.github.com>
2024-02-16 12:30:51 +0530

    custom api response and error handlling (utils files diccusstion)

--- src/db/index.js
+++ src/db/index.js
@@ -7,6 +7,7 @@ const connectDB = async () =>{
         
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`)
+       
 
     } catch (error) {
         console.log("MONGODB connection FAILED ",error);
