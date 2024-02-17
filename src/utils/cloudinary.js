d3ded9bcd3728b243afa2823ac2e9eea263a6cd4
mr-AnandShinde-358 <97289992+mr-AnandShinde-358@users.noreply.github.com>
2024-02-18 01:28:34 +0530

    add register controller and bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -28,6 +28,6 @@ const uploadOnCloudinary = async (localFilePath)=>{
 
 export {uploadOnCloudinary}
 
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+/* cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
   { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+  function(error, result) {console.log(result); }); */
