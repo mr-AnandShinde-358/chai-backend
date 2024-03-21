08e00f94b0ee4336a9c47b6ced35e3cee7311eb0
mr-AnandShinde-358 <97289992+mr-AnandShinde-358@users.noreply.github.com>
2024-03-21 14:59:34 +0530

    write dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,10 +8,130 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+    const userId = req.user?._id
+    console.log(userId)
+
+    // all likes count
+    const allLike = await Like.aggregate([
+        {
+            $match:{
+            likeBy: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $group:{
+                _id:null,
+                totalVideoLike:{
+                    $sum:{
+                        $cond:[
+                            {$ifNull:["$video",false]},
+                            1,
+                            0
+                        ]
+                    }
+                },
+                totalCommentLike:{
+                    $sum:{
+                        $cond:[
+                            {$ifNull:["$comment",false]},
+                            1,
+                            0
+                        ]
+                    }
+                },
+                totalTweetLike:{
+                    $sum:{
+                        $cond:[
+                            {$ifNull:["$tweet",false]},
+                            1,
+                            0
+                        ]
+                    }
+                }
+
+            }
+        },
+    ])
+
+    //  total subscribers
+    const totalSubscribers = await subscription.aggregate([
+        {
+            $match:{
+                channel: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $count:"totalSubscribers"
+        }
+    ])
+
+    // total videos
+
+    const totalVideos = await Video.aggregate([
+        {
+            $match:{
+                owner:new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $count:"totalVideos"
+        }
+
+    ])
+
+
+const allViews = await Video.aggregate([
+    {
+        $match:{
+            owner: new mongoose.Types.ObjectId(req.user._id)
+        }
+    },
+    {
+        $group: {
+            _id: null,
+            allVideosViews: {
+                $sum: "$views"
+            }
+        }
+    }
+])
+
+const stats = {
+    subscribers:totalSubscribers[0].totalSubscribers,
+    totalVideos:totalVideos[0].totalVideos,
+    totalVideosViews:allViews[0].allVideosViews,
+    totalVideoLikes:allLike[0].totalVideoLike,
+    totalCommentLike:allLike[0].totalCommentLike,
+    totalTweetLike:allLike[0].totalTweetLike,
+
+}
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            stats,
+            "fetching channel stats successfullY!!"
+        )
+    )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+
+    const allVideos = await Video.find({
+        owner:req.user._id
+    })
+
+    if(!allVideos){
+        throw new ApiError(500,'something went wrong fetching channel all videos !')
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,allVideos,"All Videos fetched successfully !!")
+    )
 })
 
 export {
